9544c14290362658cee4f5f619c9ef95a4c00f81
Bruno Jezek <bruno.jezek@uhk.cz>
2020-10-07 07:37:02 +0200

    Initial commit

---
+++ task1/src/rasterize/LineRasterizerTrivial.java
@@ -0,0 +1,25 @@
+package rasterize;
+
+import java.awt.*;
+
+public class LineRasterizerTrivial extends LineRasterizer{
+    public LineRasterizerTrivial(Raster raster) {
+        super(raster);
+    }
+
+    public void test(){
+        raster.setPixel(10,10, 0xFFFF00);
+    }
+
+    @Override
+    public void drawLine(int x1, int y1, int x2, int y2){
+        double k = (y2-y1)/(double)(x2-x1);
+        double q = 0;
+        for(int x = x1; x<=x2; x++) {
+            double y = k*x + q;
+            raster.setPixel(x, (int)y, 0xff0000);
+        }
+    }
+
+
+}
