fc5177834451e789552d1afd6df3cf70674415a7
simis <simi.sla@seznam.cz>
2020-10-15 23:16:10 +0200

    init

--- rasterize/LineRasterizerTrivial.java
+++ rasterize/LineRasterizerTrivial.java
@@ -12,9 +12,6 @@ public class LineRasterizerTrivial extends LineRasterizer{
     @Override
     public void drawLine(int x1, int y1, int x2, int y2){
 
-        double k = (y2-y1)/(double)(x2-x1);
-        double q = y1-k*x1;
-
         if(Math.abs(y2-y1) < Math.abs(x2-x1)){
             if(x2<x1){
                 int tmp = x1;
@@ -23,6 +20,10 @@ public class LineRasterizerTrivial extends LineRasterizer{
                 tmp = y1;
                 y1 = y2;
                 y2 = tmp;
+
+                double k = (y2-y1)/(double)(x2-x1);
+                double q = y1-k*x1;
+
                 for(int x = x1; x<=x2; x++) {
                     double y = k*x + q;
                     raster.setPixel(x, (int)y, 0xff0000);
@@ -36,6 +37,9 @@ public class LineRasterizerTrivial extends LineRasterizer{
                     y1 = y2;
                     y2 = tmp;
                 }
+                double k = (y2-y1)/(double)(x2-x1);
+                double q = y1-k*x1;
+
                 for(int y = y1; y<=y2; y++) {
                     double x = (y-q)/k;
                     raster.setPixel(y, (int)x, 0xff0000);
